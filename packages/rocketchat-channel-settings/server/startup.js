b8c6c048e1ba510a2493e81903d8afaeb3ea5875
Syed Priom <sa.priom@gmail.com>
2016-10-21 00:36:09 -0400

    manual cleanup agh

--- packages/rocketchat-channel-settings/server/startup.js
+++
@@ -1,4 +0,0 @@
-Meteor.startup(function() {
-	RocketChat.models.Permissions.upsert('post-readonly', {$set: { roles: ['admin', 'owner', 'moderator'] } });
-	RocketChat.models.Permissions.upsert('set-readonly', {$set: { roles: ['admin', 'owner'] } });
-});
